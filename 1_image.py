b6f27916e9e3623c20cce7151d928de00be8fd04
Aravinda89 <1513223865@qq.com>
2020-10-08 23:30:00 +0800

    create video.py

--- 1_image.py
+++ 1_image.py
@@ -1,7 +1,15 @@
 import cv2
 
 img = cv2.imread('./dali.jpg')
-print(img.shape)
-cv2.imshow('Image',img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# print(img.shape)
+
+b=img[:,:,0]
+print(b)
+g=img[:,:,1]
+print(g)
+r=img[:,:,2]
+print(r)
+
+# cv2.imshow('Image',img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
