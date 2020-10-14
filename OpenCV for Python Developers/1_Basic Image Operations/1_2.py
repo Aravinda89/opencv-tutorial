56691fe74e4444b13b02c685273a1b5087773724
Aravinda89 <1513223865@qq.com>
2020-10-14 21:42:21 +0800

    Update 1_2.py

--- OpenCV for Python Developers/1_Basic Image Operations/1_2.py
+++ OpenCV for Python Developers/1_Basic Image Operations/1_2.py
@@ -2,4 +2,10 @@ import cv2
 import numpy as np
 
 img = cv2.imread('opencv-logo.png', 1)
+
 print(img)
+print(type(img))
+print(img.shape)
+print(img.dtype)
+print(img[10, 5])
+print(img.size) # Total pixels
