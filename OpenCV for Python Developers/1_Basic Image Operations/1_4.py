f7d1f07d3fd84686fffb2df7d107e78708803a1a
Aravinda89 <1513223865@qq.com>
2020-10-15 22:21:29 +0800

    Update 1_4.py

--- OpenCV for Python Developers/1_Basic Image Operations/1_4.py
+++ OpenCV for Python Developers/1_Basic Image Operations/1_4.py
@@ -7,5 +7,15 @@ cv2.moveWindow('Image',0,0)
 print(color.shape)
 height, width, channel = color.shape
 
+b,g,r = cv2.split(color)
+
+rgb_split = np.empty([height,width*3,3],'uint8')
+rgb_split[:,0:width] = cv2.merge([b,b,b])
+rgb_split[:,width:width*2] = cv2.merge([g,g,g])
+rgb_split[:,width*2:width*3] = cv2.merge([r,r,r])
+
+cv2.imshow('channels',rgb_split)
+cv2.moveWindow('channels',0,height)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
