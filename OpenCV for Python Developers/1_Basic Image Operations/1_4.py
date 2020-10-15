e626d2546181c649658381f4f7b3d87e0cad86e3
Aravinda89 <1513223865@qq.com>
2020-10-15 22:02:43 +0800

    initialized

---
+++ OpenCV for Python Developers/1_Basic Image Operations/1_4.py
@@ -0,0 +1,11 @@
+import numpy as np
+import cv2
+
+color = cv2.imread('butterfly.jpg',1)
+cv2,imshow('Image',color)
+cv2.moveWindow('Image',0,0)
+print(color.shape)
+height, width, channel = color.shape
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
