4b66e02940d960b6b839dec5a0a0d4ab744ebb1c
Aravinda89 <1513223865@qq.com>
2020-10-16 22:09:54 +0800

    Update 1_5.py

--- OpenCV for Python Developers/1_Basic Image Operations/1_5.py
+++ OpenCV for Python Developers/1_Basic Image Operations/1_5.py
@@ -6,7 +6,7 @@ color = cv2.imread('butterfly.jpg', 1)
 gray  = cv2.cvtColor(color, cv2.COLOR_RGB2GRAY)
 cv2.imwrite('gray.jpg', gray)
 
-# RGBA Alpha
+# RGBA Alpha layer
 b = color[:, :, 0]
 g = color[:, :, 1]
 r = color[:, :, 2]
