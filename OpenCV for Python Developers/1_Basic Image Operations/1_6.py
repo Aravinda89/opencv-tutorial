55481d3304db3e35e87d1a7de89928c67925ce31
Aravinda89 <1513223865@qq.com>
2020-10-20 00:18:15 +0800

    Update 1_6.py

--- OpenCV for Python Developers/1_Basic Image Operations/1_6.py
+++ OpenCV for Python Developers/1_Basic Image Operations/1_6.py
@@ -1,4 +1,5 @@
-import numpy
+# Gaussian Dialte Erode
+import numpy as np
 import cv2
 
 image = cv2.imread('thresh.jpg')
@@ -6,6 +7,14 @@ cv2.imshow('Original', image)
 
 blur = cv2.GaussianBlur(image, (55,5), 0)
 cv2.imshow('Blur', blur)
+
+kernel = np.ones((5,5), 'uint8') #(5,5) ODD
+
+dilate = cv2.dilate(image, kernel, iterations=1)
+erode = cv2.erode(image, kernel, iterations=1)
+
+cv2.imshow('Dilate', dilate)
+cv2.imshow('Erode', erode)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
