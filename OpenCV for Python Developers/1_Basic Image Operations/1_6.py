fc9b41d74c143ffd833b04404f15e93256cd1e98
Aravinda89 <1513223865@qq.com>
2020-10-20 00:08:14 +0800

    Update 1_6.py

--- OpenCV for Python Developers/1_Basic Image Operations/1_6.py
+++ OpenCV for Python Developers/1_Basic Image Operations/1_6.py
@@ -4,7 +4,7 @@ import cv2
 image = cv2.imread('thresh.jpg')
 cv2.imshow('Original', image)
 
-blur = cv2.GaussianBlur(image, (5,55), 0)
+blur = cv2.GaussianBlur(image, (55,5), 0)
 cv2.imshow('Blur', blur)
 
 cv2.waitKey(0)
