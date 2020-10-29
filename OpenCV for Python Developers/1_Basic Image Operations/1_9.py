6e10b8fc2401d6948b4dda624599c9ef3d33a232
Aravinda89 <1513223865@qq.com>
2020-10-29 22:43:46 +0800

    Update 1_9.py

--- OpenCV for Python Developers/1_Basic Image Operations/1_9.py
+++ OpenCV for Python Developers/1_Basic Image Operations/1_9.py
@@ -10,6 +10,15 @@ line_width = 3
 radius = 100
 point = (0,0)
 
+def click(event, x, y, flags, param):
+    global point, pressed
+    if event == cv2.EVENT_LBUTTONDOWN:
+        print('Pressed',x,y)
+        point = (x,y)
+
+cv2.namedWindow('Frame')
+cv2.setMouseCallback('Frame',click)
+
 while(True):
     ret, frame = cap.read()
     frame  = cv2.resize(frame, (0,0),  fx=0.5, fy=0.5)
