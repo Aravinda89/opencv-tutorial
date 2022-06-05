4b5fd1580d18f4ad1c54c4c2c2e91ea9d85efa8d
Aravinda89 <1513223865@qq.com>
2022-06-05 18:03:43 +0800

    Update image_END.py
    update

--- 04_01/image_END.py
+++ 04_01/image_END.py
@@ -4,8 +4,10 @@ img = cv2.imread('../images/typewriter.jpg')
 #print(img.shape)
 
 all_rows = open('../model/synset_words.txt').read().strip().split("\n")
+print(all_rows[:2])
 
 classes = [r[r.find(' ') + 1:] for r in all_rows]
+print(classes[:2])
 
 for (i,c) in enumerate(classes):
     if i==4:
